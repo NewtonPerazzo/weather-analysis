4c34e1bab56e5854f575346aef9df50b31b6c786
Newton Perazzo <perazzoneto99@hotmail.com>
2026-08-18 09:25:40 -0300

    Fix environment for redis

--- config/redis.py
+++ config/redis.py
@@ -2,20 +2,22 @@ import json
 
 import redis
 
-redis_client = redis.Redis(
-    host="redis",
-    port=6379,
-    decode_responses=True,
-)
+from config.settings import get_settings
 
-def set_data_redis(key: str, value, time: int) -> None:
+settings = get_settings()
+
+redis_client = redis.Redis.from_url(settings.redis_url, decode_responses=True)
+
+
+def set_data_redis(key: str, value: str, time: int) -> None:
     redis_client.set(
         key,
         value,
         ex=time
     )
 
-def get_data_redis(key: str):
+
+def get_data_redis(key: str) -> dict | None:
     data = redis_client.get(key)
     if data:
         return json.loads(data)
